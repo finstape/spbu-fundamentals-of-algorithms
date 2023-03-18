da313922a28529203d678200afe84826d7579607
finstape <finstape@mail.ru>
2023-03-18 16:15:25 +0300

    solved practicum 3

--- practicum_3/homework/mst.py
+++ practicum_3/homework/mst.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+from heapq import heappush, heappop
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -10,10 +10,27 @@ def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
     mst_set = set()  # set of nodes included into MST
     rest_set = set(G.nodes())  # set of nodes not yet included into MST
     mst_edges = set()  # set of edges constituting MST
+    heap = [(0, start_node, None)]
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    # prime's algorithm
+    while heap:
+        _, current_vertex, predecessor = heappop(heap)
+
+        if current_vertex not in rest_set:
+            continue
+
+        rest_set.remove(current_vertex)
+
+        if predecessor is not None:
+            mst_edges.add((current_vertex, predecessor))
+
+        mst_set.add(current_vertex)
+
+        # add the neighbors of the current node to the heap
+        for neighbor, weight in G[current_vertex].items():
+            if neighbor not in rest_set:
+                continue
+            heappush(heap, (weight["weight"], neighbor, current_vertex))
 
     return mst_edges
 
--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -1,16 +1,36 @@
 from typing import Any
-
 import networkx as nx
+from heapq import heappush, heappop
 
 from src.plotting import plot_graph
 
 
 def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
     shortest_paths = {}  # key = destination node, value = list of intermediate nodes
+    distances = {vertex: float("inf") for vertex in G}
+    distances[source_node] = 0
+    heap = [(0, source_node)]
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    # dijkstra algorithm based on binary heap
+    while heap:
+        current_distance, current_vertex = heappop(heap)
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        # iterate over neighbors of current vertex to find minimum distance
+        for neighbor, weight in G[current_vertex].items():
+            new_distance = current_distance + weight["weight"]
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                shortest_paths[neighbor] = shortest_paths.get(current_vertex, []) + [current_vertex]
+                heappush(heap, (new_distance, neighbor))
+
+    # building the shortest path between two vertex
+    shortest_paths[source_node] = [source_node]
+    for vertex in G:
+        if vertex != source_node:
+            shortest_paths[vertex] += [vertex]
 
     return shortest_paths
 
