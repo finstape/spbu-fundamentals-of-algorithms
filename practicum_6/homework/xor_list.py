387fd003d37c5e18cc012be79511bd55d65ebbcb
finstape <finstape@mail.ru>
2023-04-16 21:28:56 +0300

    solved practicum 6 with xor list

--- practicum_6/homework/xor_list.py
+++ practicum_6/homework/xor_list.py
@@ -12,41 +12,50 @@ class Element:
     data: Any = None
     np: int = 0
 
-    def next(self, prev_el: Element) -> Element:
-        if prev_el is None:
-            return ctypes.cast(0 ^ id(self), ctypes.py_object).value
-        return ctypes.cast(prev_el.np ^ id(self), ctypes.py_object).value
+    def next(self, prev_p: int) -> int:
+        return self.np ^ prev_p
 
-    def prev(self, next_el: Element) -> Element:
-        if next_el is None:
-            return ctypes.cast(0 ^ id(self), ctypes.py_object).value
-        return ctypes.cast(next_el.np ^ id(self), ctypes.py_object).value
+    def prev(self, next_p: int) -> int:
+        return self.np ^ next_p
 
 
 class XorDoublyLinkedList:
     def __init__(self) -> None:
         self.head: Element = None
         self.tail: Element = None
+        self.__nodes: list[Element] = []
 
     def __repr__(self) -> str:
         return str(self)
 
     def __str__(self) -> str:
+        if self.empty():
+            return ""
+
         node_keys = []
-        next_el: Element = self.head
-        prev_el: Element = None
-        while next_el is not None:
+        next_id = id(self.head)
+        prev_id = 0
+
+        while next_id != 0:
+            next_el = ctypes.cast(next_id, ctypes.py_object).value
             node_keys.append(str(next_el.key))
-            prev_el, next_el = next_el, next_el.next(prev_el)
+            prev_id, next_id = next_id, next_el.next(prev_id)
+
         return " <-> ".join(node_keys)
 
     def to_pylist(self) -> list[Any]:
+        if self.empty():
+            return []
+
         py_list = []
-        next_el: Element = self.head
-        prev_el: Element = None
-        while next_el is not None:
+        next_id = id(self.head)
+        prev_id = 0
+
+        while next_id != 0:
+            next_el = ctypes.cast(next_id, ctypes.py_object).value
             py_list.append(next_el.key)
-            prev_el, next_el = next_el, next_el.next(prev_el)
+            prev_id, next_id = next_id, next_el.next(prev_id)
+
         return py_list
 
     def empty(self):
@@ -54,10 +63,21 @@ class XorDoublyLinkedList:
 
     def search(self, k: Element) -> Element:
         """Complexity: O(n)"""
-        next_el: Element = self.head
-        prev_el: Element = None
-        while next_el is not None and next_el != k:
-            prev_el, next_el = next_el, next_el.next(prev_el)
+        if self.empty():
+            raise ValueError("List is empty!")
+
+        next_id = id(self.head)
+        prev_id = 0
+        next_el = self.head
+
+        while next_id != 0 and next_el.key != k.key:
+            prev_id, next_id = next_id, next_el.next(prev_id)
+
+            if next_id == 0:
+                raise ValueError("List doesn't store this element!")
+
+            next_el = ctypes.cast(next_id, ctypes.py_object).value
+
         return next_el
 
     def insert(self, x: Element) -> None:
@@ -67,31 +87,75 @@ class XorDoublyLinkedList:
         if self.head is None:
             self.head = self.tail = x
         else:
-            x.np = id(self.head) ^ 0
-            self.head.np = id(x) ^ self.head.np
+            x.np = id(self.head)
+            self.head.np = self.head.np ^ id(x)
             self.head = x
+        self.__nodes = [x] + self.__nodes
 
     def remove(self, x: Element) -> None:
         """Remove x from the list
-        Complexity: O(1)
+        Complexity: O(n)
         """
+        if self.empty():
+            raise ValueError("List is empty!")
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        if self.head.key == x.key:
+            if self.head == self.tail:
+                self.__nodes.remove(self.head)
+                self.head = self.tail = None
+            else:
+                if self.tail == ctypes.cast(self.head.next(0), ctypes.py_object).value:
+                    self.head = self.tail
+                    self.head.np = self.tail.np = 0
+                    self.__nodes = [self.head]
+                else:
+                    next_el = ctypes.cast(self.head.next(0), ctypes.py_object).value
+                    NEXT_EL = ctypes.cast(next_el.next(id(self.head)), ctypes.py_object).value
+                    self.head = next_el
+                    self.head.np = 0 ^ id(NEXT_EL)
+                    self.__nodes.pop(0)
+        elif self.tail.key == x.key:
+            if self.head == ctypes.cast(self.tail.prev(0), ctypes.py_object).value:
+                self.tail = self.head
+                self.head.np = self.tail.np = 0
+                self.__nodes = [self.tail]
+            else:
+                next_el = ctypes.cast(self.head.prev(0), ctypes.py_object).value
+                NEXT_EL = ctypes.cast(next_el.next(id(self.tail)), ctypes.py_object).value
+                self.tail = next_el
+                self.tail.np = 0 ^ id(NEXT_EL)
+                self.__nodes.pop(len(self.__nodes) - 1)
+        else:
+            next_id = id(self.head)
+            prev_id = 0
+            next_el = self.head
 
-        pass
+            while next_id != 0 and next_el.key != x.key:
+                prev_id, next_id = next_id, next_el.next(prev_id)
+
+                if next_id == 0:
+                    raise ValueError("List doesn't store this element!")
+
+                next_el = ctypes.cast(next_id, ctypes.py_object).value
+
+            prev_el = ctypes.cast(prev_id, ctypes.py_object).value
+            NEXT_ID = next_el.np ^ prev_id
+            NEXT_EL = ctypes.cast(NEXT_ID, ctypes.py_object).value
+
+            prev_el.np = prev_el.np ^ next_id ^ NEXT_ID
+            NEXT_EL.np = NEXT_EL.np ^ next_id ^ prev_id
+
+            self.__nodes.remove(next_el)
 
     def reverse(self) -> XorDoublyLinkedList:
         """Returns the same list but in the reserved order
         Complexity: O(1)
         """
+        if self.empty():
+            raise ValueError("List is empty!")
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        self.head, self.tail = self.tail, self.head
+        return self
 
 
 if __name__ == "__main__":
